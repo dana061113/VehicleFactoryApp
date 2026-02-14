5a812d528615c32c44a777f07563c308d5ae4b0b
dana061113 <danakobylandykyzy@gmail.com>
2026-02-14 20:25:56 +0500

    Add Car class implementing IVehicle interface

---
+++ vehicles/Car.java
@@ -0,0 +1,26 @@
+package vehicles;
+
+import interfaces.IVehicle;
+
+public class Car implements IVehicle {
+
+    private String brand;
+    private String model;
+    private String fuelType;
+
+    public Car(String brand, String model, String fuelType) {
+        this.brand = brand;
+        this.model = model;
+        this.fuelType = fuelType;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Автомобиль " + brand + " " + model + " қозғалып жатыр.");
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("Жанармай түрі: " + fuelType);
+    }
+}
